da0695b343416ee87549867c27b09c88c207ab87
suraj-2021 <madwilly6@gmail.com>
2023-09-15 18:38:45 +0530

    Added 3 database Tables, Created Super User, One to One, One to Many and Many to Many relations etc..."

--- myclub_website/events/admin.py
+++ myclub_website/events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import MyClubUser
+from . models import Venue
+from .models import Event
 # Register your models here.
+
+admin.site.register(MyClubUser)
+admin.site.register(Venue)
+admin.site.register(Event)
