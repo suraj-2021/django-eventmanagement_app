21a603270da905367c2dc3173110d0bbaefd5659
suraj-2021 <madwilly6@gmail.com>
2023-09-19 08:18:51 +0530

    Made Changes in Admin Area, Changed the ordering of data, modified the registerd admins etc...

--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-
-from django.db import models
+from django.contrib.auth.models import User
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField(max_length=15)
-    phone = models.CharField('Contact Phone', max_length=50)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=50, blank = True)
+    web = models.URLField('Website Address', blank = True)
+    email_address = models.EmailField('Email Address', blank = True)
 
     def __str__(self):
         return self.name
@@ -25,9 +24,10 @@ class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.CharField(max_length = 120) # Change this to a ForeignKey
-    description = models.TextField(blank=True)
+    #manager = models.CharField(max_length = 120)
+    manager = models.ForeignKey(User, blank = True, null = True, on_delete = models.SET_NULL)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
+    description = models.TextField(blank = True, null = True)
 
     def __str__(self):
         return self.name
